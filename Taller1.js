39d60a324fc70bda8588ea23c45e94e8b57e70c8
SakaeHiguchi <sakaehiguchi.nareth@gmail.com>
2022-03-01 11:22:56 -0500

    sexo

---
+++ Taller1.js
@@ -0,0 +1,26 @@
+//Archivo de práctica y pruebas de JavaScript
+/*Taller 1
+Fecha 21/02/2022
+Realizado por: nombre_de_estudiante(Sakae Higuchi)
+*/
+var nombre;//string
+console.log(nombre);
+var grado;//string
+console.log(grado);
+
+var edad;//int
+console.log(edad);//desde teclado 
+
+var grado="10B"
+var nombre= "Sakae Higuchi"
+console.log("el grado es:" + grado);
+//imprimir console
+//simple
+console.log("mensaje");
+console.log(grado);
+//concatenado
+console.log("El estudiante "+nombre+" del grado "+grado+" tiene "+edad)
+
+
+
+
---
+++ index.js
@@ -0,0 +1,63 @@
+console.log("hi");//do");//imprimir por consola
+
+
+//comentario en linea
+/*comentario
+en
+varias
+lineas 
+o
+multilinea
+*/
+
+
+//variables
+/*son espacios de memoria que almacenan datos o informacion 
+existen varios tipos de variables en js, pero generalmente se utilizan los saiguientes:
+string:almacena cadenas de textos. se debe declarar o asignar el valor entre "comillas"
+int:almacena numeros enteros
+double:almacena numeros reales
+boolean:almacenan valores de verdad (true,false)
+*/
+/*declarar variables
+declarar una variable es crearla,decirle al lenguaje como se llama la variable he indicarle que ya existe.
+para declarar una variable se puede utilizar ya sea la palabra reservada var o let 
+*/
+var nombre;
+console.log(nombre);
+/*inicializar variables
+para inicializar variables es necesario asiganar un valor cuando se crea esta variable
+*/
+var apellido="Higuchi";
+console.log(apellido)
+/*asignar un valor: asignar un valor se hace mediante el signo =, se debe hacer sobre una variable que ya fue declarada
+*/
+nombre="Sakae";
+console.log(nombre+" "+apellido)
+
+
+//desde otra variable 
+var edad=16;
+var edadEstudiante=edad;
+//desde el teclado 
+var gradoEstudiante=prompt("ingrese un grado");
+console.log("el grado es:"+gradoEstudiante);
+/*constantes: son valores que una vez inicializados no pueden cambiar lo que almacenan. se crean con la palabra reservada const, el nombre de las constantes se describe todo en mayusculas para diferenciarlo de las variables
+*/
+const PI=3.1416;
+//PT=5;
+//imprimir por console 
+//simple
+console.log("mensaje");
+console.log(nombre);
+//concatenado
+console.log("la edad es "+edad+" años");
+
+
+
+
+
+
+
+
+
