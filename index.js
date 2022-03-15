99353e857376819e30335eec59c57d30654ac056
SakaeHiguchi <sakaehiguchi.nareth@gmail.com>
2022-03-15 11:24:15 -0500

    Solución taller 2

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ console.log("hi");//do");//imprimir por consola
 /*comentario
 en
 varias
-lineas 
+lineas
 o
 multilinea
 */
 
 
 //variables
-/*son espacios de memoria que almacenan datos o informacion 
+/*son espacios de memoria que almacenan datos o informacion
 existen varios tipos de variables en js, pero generalmente se utilizan los saiguientes:
 string:almacena cadenas de textos. se debe declarar o asignar el valor entre "comillas"
 int:almacena numeros enteros
@@ -21,7 +21,7 @@ boolean:almacenan valores de verdad (true,false)
 */
 /*declarar variables
 declarar una variable es crearla,decirle al lenguaje como se llama la variable he indicarle que ya existe.
-para declarar una variable se puede utilizar ya sea la palabra reservada var o let 
+para declarar una variable se puede utilizar ya sea la palabra reservada var o let
 */
 var nombre;
 console.log(nombre);
@@ -36,17 +36,17 @@ nombre="Sakae";
 console.log(nombre+" "+apellido)
 
 
-//desde otra  variable 
+//desde otra  variable
 var edad=16;
 var edadEstudiante=edad;
-//desde el teclado 
-var gradoEstudiante=prompt("ingrese un grado");
-console.log("el grado es:"+gradoEstudiante);
+//desde el teclado
+// var gradoEstudiante=prompt("ingrese un grado");
+// console.log("el grado es:"+gradoEstudiante);
 /*constantes: son valores que una vez inicializados no pueden cambiar lo que almacenan. se crean con la palabra reservada const, el nombre de las constantes se describe todo en mayusculas para diferenciarlo de las variables
 */
 const PI=3.1416;
 //PT=5;
-//imprimir por console 
+//imprimir por console
 //simple
 console.log("mensaje");
 console.log(nombre);
@@ -55,7 +55,7 @@ console.log("la edad es "+edad+" años");
 
 
 //operadores
-//suma 
+//suma
 let a = 10;
 let b=20;
 var suma= a+b;
@@ -65,10 +65,76 @@ var resultado;
 resultado= a-b;
 console.log(resultado);
 resultado=a-b;
-//multiplicacion 
+//multiplicacion
 var producto;
 producto=a*b;
 console.log(producto);
+//division
+var divisiones
+division =b/a
+console.log(division);
+//orden de operadores
+var res;
+console.log(res);
+res=a*(2-1);//10
+console.log(res);
+
+//Operaciones Aumentadas
+//son operaciones que se le realizan a una variable y el resultado se guarda en si misma. la variable debe tener un valor antes de realizar la operacion
+
+var sumaAumento=0;
+sumaAumento+=48;
+sumaAumento+=1;
+console.log(sumaAumento);
+
+var productoAumento=20;
+productoAumento*=1;
+productoAumento*=8
+console.log(productoAumento);
+
+
+var cocienteAumento=20;
+cocienteAumento/=1;
+cocienteAumento/=8
+console.log(cocienteAumento);
+
+//potencia
+var exp,
+exp=3**2;//3^2
+console.log(exp);//9
+exp=3**3;//3^3
+console.log(exp);//27
+
+//cadenas o textos
+//suma de cadenas
+var nombre="Sakae";
+var apellido="Higuchi" ;
+var nombreCompleto=nombre+" "+apellido;
+console.log(nombreCompleto);
+
+ var cadenita1="3";
+ var cadenita2="5";
+ var cadenaTotal=cadenita1+cadenita2;
+ console.log(cadenaTotal);
+
+ //.length
+ //permite conocer la longitud de una cadena o el tamaño de un vector
+ console.log(nombreCompleto.length);
+ console.log(nombre[3]);//a
+ //para encontrar el caracter en la ultima posicion de una cadena o un vector se debe buscar la longtitud -1(.length-1)
+ console.log (nombre[nombre.length-1]);
+ 
+
+
+
+
+
+ 
+
+
+
+
+
 
 
 
---
+++ taller2.js
@@ -0,0 +1,46 @@
+var Lado=prompt("Por favor ingrese la medida del lado para hallar el área del cuadrado");
+var areacuadrado=Lado**2;
+console.log(areacuadrado);
+
+var Volumen=prompt("por favor ingrese el lado del cubo para hallar el Volumen");
+var areaVolumen=Volumen**3;
+console.log(areaVolumen);
+
+var base=prompt("por favor ingrese la base para hallar el area del rectangulo");
+var AreaRectangulo=base*altura
+var altura=prompt("por favor ingrese la altura para hallar el area del rectangulo");
+console.log(AreaRectangulo);
+ 
+
+var l=prompt("por favor ingrese el valor del lado ");
+var a=prompt("por favor ingrese el valor de la altura ");
+var Perimetro=l+a+l+a
+console.log(Perimetro);
+
+var Circulo=prompt("por favor ingrese el diametro del circulo");
+var Radio=Circulo/2
+var AreaCirtulo=Math.PI*Radio**2;
+console.log(AreaCirtulo);
+
+var Volumen=4/2*Math.PI*Radio**2;
+console.log(Volumen);
+
+var base1=prompt("por favor ingrese el valor de base1");
+var base2=prompt("por favor ingrese el valor de base2");
+var h=prompt("por favor ingrese el valor de h");
+console.log("el area de un trapezoide es: " +(h * (base1 + base2)/ 2)) //calificar la formula
+
+var ancho;
+var largo;
+var profundo;
+ancho=parseInt(profundo("por favor ingresar el ancho"));
+largo=parseInt(prompt("por favor ingresar el largo"));
+profundo=parseInt(prompt("por favor ingrese la profundidad"));
+console.log("la piscina se llena con ",ancho * largo * profundo,"litros")
+
+
+var totalApagar=prompt("por favor ingrese el valor de alto");
+var totalApagar=prompt("por favor ingrese el valor de ancho");
+var totalApagar=alto*ancho*22000
+console.log(totalApagar);
+
